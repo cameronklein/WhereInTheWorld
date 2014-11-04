8c44394296249a7f516d32fafbd3bbff435aae4f
Cameron Klein <cameron.klein@gmail.com>
2014-11-04 14:44:56 -0800

    Initial Commit

---
+++ Where In The World?Tests/Where_In_The_World_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Where_In_The_World_Tests.swift
+//  Where In The World?Tests
+//
+//  Created by Cameron Klein on 11/4/14.
+//  Copyright (c) 2014 Cameron Klein. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Where_In_The_World_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
